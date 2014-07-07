c41761dba574cb242011b2038d4f404288b4835f
Tim Tregubov <tim@zingweb.com>
2014-07-06 21:23:36 -0400

    working perceptual shifts

--- Assets/Codes/CameraRayCaster.cs
+++ Assets/Codes/CameraRayCaster.cs
@@ -3,8 +3,33 @@ using System.Collections;
 
 public class CameraRayCaster : MonoBehaviour {
 
+	private Ray ray;
+	private RaycastHit hit;
+	private Transform camTransform;
+	private GameObject lastGO; 
+
+	void Start ()
+	{
+		camTransform = Camera.main.transform;
+	}
+
 	void Update () 
 	{
-	
+		ray = new Ray(camTransform.position, camTransform.forward);
+
+		if (Physics.Raycast(ray, out hit, 100f)) 
+		{
+			if ( hit.transform.gameObject.Equals(lastGO) )
+			{
+				hit.transform.SendMessage("Seen", SendMessageOptions.DontRequireReceiver);
+			}
+			else
+			{
+				hit.transform.SendMessage("Unseen", SendMessageOptions.DontRequireReceiver);
+			}
+
+			lastGO = hit.transform.gameObject;
+
+		}
 	}
 }
---
+++ Assets/Codes/PercetualReceiver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PercetualReceiver : MonoBehaviour {
+
+	public Material MaterialToEffect;
+	public Light LightToEffect;
+
+	public Color MaterialTargetColor;
+	public Color LightTargetColor;
+
+	public float seen;
+	public float step = 0.001f;
+
+	void Start ()
+	{
+		seen = 0f;
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	public void Seen ()
+	{
+		seen += step * Time.deltaTime;
+		MaterialToEffect.color = Color.Lerp(MaterialToEffect.color, MaterialTargetColor, seen);
+		LightToEffect.color = Color.Lerp(LightToEffect.color, LightTargetColor, seen);
+	}
+
+	public void Unseen ()
+	{
+		seen = 0;
+	}
+}
